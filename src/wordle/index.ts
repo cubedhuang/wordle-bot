1db2c2b9696b2eec66a81e3ae2d711175ae06948
cubedhuang <99danielh@gmail.com>
2022-05-31 17:01:49 +0000

    refactor: use plain object for trie with count

--- src/wordle/index.ts
+++ src/wordle/index.ts
@@ -1,33 +1,42 @@
 import { words } from "./words";
 
-class TrieNode {
-	children?: Record<string, TrieNode>;
-
-	add(word: string) {
-		if (word.length === 0) return;
-		this.children ??= {};
-		this.children[word[0]] ??= new TrieNode();
-		this.children[word[0]].add(word.slice(1));
-	}
-
-	has(word: string): boolean {
-		if (word.length === 0) return true;
-		if (!this.children?.[word[0]]) return false;
-		return this.children[word[0]].has(word.slice(1));
-	}
+interface TrieNode {
+	[key: string]: TrieNode;
 }
 
-const root = new TrieNode();
+function trieInsert(node: TrieNode, word: string) {
+	if (word.length === 0) return;
+	node[word[0]] ??= {};
+	trieInsert(node[word[0]], word.slice(1));
+}
+
+function trieHas(node: TrieNode, word: string): boolean {
+	if (word.length === 0) return true;
+	if (!node[word[0]]) return false;
+	return trieHas(node[word[0]], word.slice(1));
+}
+
+function trieCount(node: TrieNode): number {
+	if (Object.keys(node).length === 0) return 1;
+	return Object.values(node).reduce((acc, node) => acc + trieCount(node), 0);
+}
+
+const root: TrieNode = {};
 
 const start = process.hrtime.bigint();
-for (const word of words.answers) root.add(word);
-for (const word of words.dictionary) root.add(word);
+for (const word of words.answers) trieInsert(root, word);
+for (const word of words.dictionary) trieInsert(root, word);
 const end = process.hrtime.bigint();
 
 console.log(`Built Wordle trie in ${Number(end - start) / 1e6}ms.`);
+console.log(
+	`${trieCount(root)} words in trie. ${words.answers.length} answers + ${
+		words.dictionary.length
+	} dictionary = ${words.answers.length + words.dictionary.length} total.`
+);
 
 export function isWordleWord(word: string) {
-	return word.length === 5 && root.has(word.toLowerCase());
+	return word.length === 5 && trieHas(root, word.toLowerCase());
 }
 
 export function getRandomWordleAnswer() {
