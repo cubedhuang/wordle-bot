dfd780242e84852599b0337d13557304d314e143
cubedhuang <99danielh@gmail.com>
2023-04-05 00:36:39 -0400

    fix shards

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,7 @@ import { isDev } from "./util.js";
 
 const manager = new ShardingManager("build/src/bot.js", {
 	shardArgs: process.argv.slice(2),
-	token: isDev ? process.env.TOKEN_DEV : process.env.TOKEN,
-
-	totalShards: 3
+	token: isDev ? process.env.TOKEN_DEV : process.env.TOKEN
 });
 
 manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}.`));
